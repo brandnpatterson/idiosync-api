0e0566ad1db8e67f0464f02c92ff50f319df6f98
Brandon Patterson <brandnpatterson@gmail.com>
2017-08-26 16:20:31 -0500

    set up data flow with acts-as-taggable-on

--- db/migrate/20170816153518_create_articles.rb
+++ db/migrate/20170816153518_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
       t.string :title
       t.string :author
       t.string :content
-      
+      t.string :tags, array: true, default: []
       t.timestamps
     end
   end
